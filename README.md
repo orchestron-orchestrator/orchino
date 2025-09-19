6e3c0c7021532a0bdd89d1c27d43075969b54dbe
Kris Lambrechts <kris@netedge.plus>
2025-09-19 13:54:10 +0200

    Simplify make recipes

--- README.md
+++ README.md
@@ -8,6 +8,38 @@ Orchestron-based orchestrator, we recommend you fork SORESPO and modify it to
 fit your needs.
 
 But, we also very much understand that the SORESPO project with its many bells
-and whissles can be a little overwhelming when you are just starting out with
+and whistles can be a little overwhelming when you are just starting out with
 developing for Orchestron. That's where Orchino comes in. Use this project as
 a playground for your first baby steps with the platform.
+
+##  Getting Started
+Orchino is really just a stripped-down version of SORESPO, so all the same
+principles still apply here.
+
+Whenever you have modified the YANG models, re-generate the Acton modules for
+your service layers:
+```shell
+make gen
+```
+
+Whenever you have modified the transform code, rebuild:
+```shell
+make build
+```
+
+Throughout your development, run the `quicklab-notconf` test environment:
+```
+cd test/quicklab-notconf
+make start copy run-and-configure
+```
+
+Note: if you have the XRd image, try the `quicklab` instead.
+
+## Files to explore
+The key files you may want look into and or modify are:
+* `gen/src/orchino_gen.act` which you'll see reference:
+  * `gen/yang/cfs/netinfra.yang`
+  * `gen/yang/rfs/orchino-rfs.yang`
+  * `gen/yang/rfs/CiscoIosXr_24_1_ncs55a1/*`
+* `src/orchino/cfs.act`
+* `src/orchino/rfs.act`
--- test/common/quicklab.mk
+++ test/common/quicklab.mk
@@ -1,29 +1,6 @@
 build-otron-image:
 	docker build -t orchino-otron-base -f ../common/Dockerfile.otron .
 
-licenses/%:
-# Ensure the symlink to the licenses private repo exists in the project root
-	@if [ ! -d ../../licenses ]; then \
-		echo "Error: licenses directory not found."; \
-		if [ ! -d ../../../licenses ]; then \
-			REMOTE_URL=$$(git remote get-url origin); \
-			LICENSES_URL="$$(echo "$$REMOTE_URL" | sed -E 's|/[^/]+$$|/licenses.git|')"; \
-			echo "Cloning licenses repository from $$LICENSES_URL"; \
-			(cd ../../.. && git clone "$$LICENSES_URL") || \
-			(echo "Failed to clone licenses repository." && exit 1); \
-		else \
-			echo "Found existing licenses repository at ../../../licenses"; \
-		fi; \
-		echo "Creating symlink to licenses directory..."; \
-		ln -s ../licenses ../../licenses || \
-		(echo "Failed to create symlink to licenses directory." && exit 1); \
-	fi
-# Copy the requested license file to the test directory. We run containerlab in
-# a container, meaning we cannot follow a symlink outside of the current
-# project directory.
-	mkdir -p licenses
-	cp ../../licenses/$* $@
-
 .PHONY: start
 start: build-otron-image
 	$(CLAB_BIN) deploy --topo $(TESTENV:orchino-%=%).clab.yml --log-level debug --reconfigure
@@ -32,9 +9,9 @@ start: build-otron-image
 stop:
 	$(CLAB_BIN) destroy --topo $(TESTENV:orchino-%=%).clab.yml --log-level debug
 
-.PHONY: wait $(addprefix wait-,$(ROUTERS_XR) $(ROUTERS_CRPD) $(ROUTERS_SRL))
+.PHONY: wait $(addprefix wait-,$(ROUTERS_XR))
 WAIT?=60
-wait: $(addprefix platform-wait-,$(ROUTERS_XR) $(ROUTERS_CRPD) $(ROUTERS_SRL))
+wait: $(addprefix platform-wait-,$(ROUTERS_XR))
 
 .PHONY: copy
 copy:
@@ -57,22 +34,6 @@ run-and-configure:
 configure:
 	$(MAKE) FILE="netinfra.xml" send-config
 
-.PHONY: tutorial
-tutorial:
-	$(MAKE) -C ../../ download-release
-	$(MAKE) start
-	$(MAKE) copy
-	$(MAKE) run
-
-.PHONY: dev-tutorial
-dev-tutorial:
-	$(MAKE) -C ../../ download-release
-	$(MAKE) start
-	# Sleeping 30 seconds to make sure SR Linux has started properly
-	sleep 30
-	$(MAKE) copy
-	$(MAKE) run-and-configure
-
 .PHONY: shell
 shell:
 	docker exec -it $(TESTENV)-otron bash -l
@@ -85,32 +46,12 @@ send-config-async:
 send-config-wait:
 	curl -X PUT -H "Content-Type: application/yang-data+xml" -d @$(FILE) http://localhost:$(shell docker inspect -f '{{(index (index .NetworkSettings.Ports "80/tcp") 0).HostPort}}' $(TESTENV)-otron)/restconf
 
-.PHONY: send-config-json-async
-send-config-json-async:
-	curl -X PUT -H "Content-Type: application/yang-data+json" -H "Async: true" -d @$(FILE) http://localhost:$(shell docker inspect -f '{{(index (index .NetworkSettings.Ports "80/tcp") 0).HostPort}}' $(TESTENV)-otron)/restconf
-
-.PHONY: send-config-json-wait
-send-config-json-wait:
-	curl -X PUT -H "Content-Type: application/yang-data+json" -d @$(FILE) http://localhost:$(shell docker inspect -f '{{(index (index .NetworkSettings.Ports "80/tcp") 0).HostPort}}' $(TESTENV)-otron)/restconf
-
-.PHONY: send-config-tmf
-send-config-tmf:
-	curl -X POST -H "Content-Type: application/json" -d @$(FILE) http://localhost:$(shell docker inspect -f '{{(index (index .NetworkSettings.Ports "80/tcp") 0).HostPort}}' $(TESTENV)-otron)/tmf-api/serviceOrdering/v4/serviceOrder
-
-.PHONY: get-config-tmf
-get-config-tmf:
-	curl -X GET http://localhost:$(shell docker inspect -f '{{(index (index .NetworkSettings.Ports "80/tcp") 0).HostPort}}' $(TESTENV)-otron)/tmf-api/serviceOrdering/v4/serviceOrder$(if $(ID),/$(ID),)
-
-.PHONY: get-config0 get-config1 get-config2 get-config3
-get-config0 get-config1 get-config2 get-config3:
+.PHONY: get-config0 get-config1 get-config2
+get-config0 get-config1 get-config2:
 	curl -H "Accept: application/yang-data+xml" http://localhost:$(shell docker inspect -f '{{(index (index .NetworkSettings.Ports "80/tcp") 0).HostPort}}' $(TESTENV)-otron)/layer/$(subst get-config,,$@)
 
-.PHONY: get-config-json0 get-config-json1 get-config-json2 get-config-json3
-get-config-json0 get-config-json1 get-config-json2 get-config-json3:
-	@curl -H "Accept: application/yang-data+json" http://localhost:$(shell docker inspect -f '{{(index (index .NetworkSettings.Ports "80/tcp") 0).HostPort}}' $(TESTENV)-otron)/layer/$(subst get-config-json,,$@)
-
-.PHONY: get-config-adata0 get-config-adata1 get-config-adata2 get-config-adata3
-get-config-adata0 get-config-adata1 get-config-adata2 get-config-adata3:
+.PHONY: get-config-adata0 get-config-adata1 get-config-adata2
+get-config-adata0 get-config-adata1 get-config-adata2:
 	@curl -H "Accept: application/yang-data+acton-adata" http://localhost:$(shell docker inspect -f '{{(index (index .NetworkSettings.Ports "80/tcp") 0).HostPort}}' $(TESTENV)-otron)/layer/$(subst get-config-adata,,$@)
 
 # Default headers for XML configuration
@@ -119,60 +60,56 @@ HEADERS?=-H "Accept: application/yang-data+xml"
 # "target" is the Orchestron's intended configuration, i.e. the configuration
 # *we* want on the device. Note how this is not NMDA-speak for "intended
 # configuration" of the device itself.
-.PHONY: $(addprefix get-target-,$(ROUTERS_XR) $(ROUTERS_CRPD) $(ROUTERS_SRL))
-$(addprefix get-target-,$(ROUTERS_XR) $(ROUTERS_CRPD) $(ROUTERS_SRL)):
+.PHONY: $(addprefix get-target-,$(ROUTERS_XR))
+$(addprefix get-target-,$(ROUTERS_XR)):
 	@curl $(HEADERS) http://localhost:$(shell docker inspect -f '{{(index (index .NetworkSettings.Ports "80/tcp") 0).HostPort}}' $(TESTENV)-otron)/device/$(subst get-target-,,$@)/target
 
-.PHONY: $(addprefix get-target-adata-,$(ROUTERS_XR) $(ROUTERS_CRPD) $(ROUTERS_SRL))
-$(addprefix get-target-adata-,$(ROUTERS_XR) $(ROUTERS_CRPD) $(ROUTERS_SRL)):
+.PHONY: $(addprefix get-target-adata-,$(ROUTERS_XR))
+$(addprefix get-target-adata-,$(ROUTERS_XR)):
 	@$(MAKE) HEADERS="-H \"Accept: application/yang-data+acton-adata\"" $(subst adata-,,$@)
 
 # "running" is the currently running configuration on the device, which in
 # NMDA-speak is the "intended configuration".
-.PHONY: $(addprefix get-running-,$(ROUTERS_XR) $(ROUTERS_CRPD) $(ROUTERS_SRL))
-$(addprefix get-running-,$(ROUTERS_XR) $(ROUTERS_CRPD) $(ROUTERS_SRL)):
+.PHONY: $(addprefix get-running-,$(ROUTERS_XR))
+$(addprefix get-running-,$(ROUTERS_XR)):
 	@curl $(HEADERS) http://localhost:$(shell docker inspect -f '{{(index (index .NetworkSettings.Ports "80/tcp") 0).HostPort}}' $(TESTENV)-otron)/device/$(subst get-running-,,$@)/running
 
-.PHONY: $(addprefix get-running-adata-,$(ROUTERS_XR) $(ROUTERS_CRPD) $(ROUTERS_SRL))
-$(addprefix get-running-adata-,$(ROUTERS_XR) $(ROUTERS_CRPD) $(ROUTERS_SRL)):
+.PHONY: $(addprefix get-running-adata-,$(ROUTERS_XR))
+$(addprefix get-running-adata-,$(ROUTERS_XR)):
 	@$(MAKE) HEADERS="-H \"Accept: application/yang-data+acton-adata\"" $(subst adata-,,$@)
 
-.PHONY: $(addprefix get-running-diff-,$(ROUTERS_XR) $(ROUTERS_CRPD) $(ROUTERS_SRL))
-$(addprefix get-running-diff-,$(ROUTERS_XR) $(ROUTERS_CRPD) $(ROUTERS_SRL)):
+.PHONY: $(addprefix get-running-diff-,$(ROUTERS_XR))
+$(addprefix get-running-diff-,$(ROUTERS_XR)):
 	@curl $(HEADERS) http://localhost:$(shell docker inspect -f '{{(index (index .NetworkSettings.Ports "80/tcp") 0).HostPort}}' $(TESTENV)-otron)/device/$(subst get-running-diff-,,$@)/diff
 
-.PHONY: $(addprefix resync-,$(ROUTERS_XR) $(ROUTERS_CRPD) $(ROUTERS_SRL))
-$(addprefix resync-,$(ROUTERS_XR) $(ROUTERS_CRPD) $(ROUTERS_SRL)):
+.PHONY: $(addprefix resync-,$(ROUTERS_XR))
+$(addprefix resync-,$(ROUTERS_XR)):
 	@curl $(HEADERS) http://localhost:$(shell docker inspect -f '{{(index (index .NetworkSettings.Ports "80/tcp") 0).HostPort}}' $(TESTENV)-otron)/device/$(subst resync-,,$@)/resync
 
 .PHONY: delete-config
 delete-config:
 	curl -X DELETE http://localhost:$(shell docker inspect -f '{{(index (index .NetworkSettings.Ports "80/tcp") 0).HostPort}}' $(TESTENV)-otron)/restconf/netinfra:netinfra/routers=STO-CORE-1
 
-.PHONY: $(addprefix cli-,$(ROUTERS_XR) $(ROUTERS_CRPD) $(ROUTERS_SRL) $(ROUTERS_FRR))
-$(addprefix cli-,$(ROUTERS_XR) $(ROUTERS_CRPD) $(ROUTERS_SRL) $(ROUTERS_FRR)): cli-%: platform-cli-%
+.PHONY: $(addprefix cli-,$(ROUTERS_XR))
+$(addprefix cli-,$(ROUTERS_XR)): cli-%: platform-cli-%
 
-.PHONY: $(addprefix get-dev-config-,$(ROUTERS_XR) $(ROUTERS_CRPD) $(ROUTERS_SRL))
-$(addprefix get-dev-config-,$(ROUTERS_XR) $(ROUTERS_CRPD) $(ROUTERS_SRL)):
+.PHONY: $(addprefix get-dev-config-,$(ROUTERS_XR))
+$(addprefix get-dev-config-,$(ROUTERS_XR)):
 	docker run $(INTERACTIVE) --rm --network container:$(TESTENV)-otron ghcr.io/notconf/notconf:debug netconf-console2 --host $(@:get-dev-config-%=%) --port 830 --user clab --pass clab@123 --get-config
 
 .PHONY: test
 test:
-	$(MAKE) test-ping
 	$(MAKE) test-get-config
-
-.PHONY: test-ping
-test-ping::
 
 .PHONY: test-get-config
 test-get-config:
-	$(MAKE) $(addprefix get-dev-config-,$(ROUTERS_XR) $(ROUTERS_CRPD) $(ROUTERS_SRL))
+	$(MAKE) $(addprefix get-dev-config-,$(ROUTERS_XR))
 
 .PHONY: save-logs
-save-logs: $(addprefix save-logs-,$(ROUTERS_XR) $(ROUTERS_CRPD) $(ROUTERS_SRL))
+save-logs: $(addprefix save-logs-,$(ROUTERS_XR))
 
-.PHONY: $(addprefix save-logs-,$(ROUTERS_XR) $(ROUTERS_CRPD) $(ROUTERS_SRL))
-$(addprefix save-logs-,$(ROUTERS_XR) $(ROUTERS_CRPD) $(ROUTERS_SRL)):
+.PHONY: $(addprefix save-logs-,$(ROUTERS_XR))
+$(addprefix save-logs-,$(ROUTERS_XR)):
 	mkdir -p logs
 	docker logs --timestamps $(TESTENV)-$(@:save-logs-%=%) > logs/$(@:save-logs-%=%)_docker.log 2>&1
 	$(MAKE) get-dev-config-$(@:save-logs-%=%) > logs/$(@:save-logs-%=%)_netconf.log || true
